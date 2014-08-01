9ff1a48021dcab25a71f32a38ace7be1509de5c0
DuncanUszkay <duncan.uszkay@hotmail.com>
2014-08-01 08:44:02 -0700

    Delete dictscript.py

--- dictscript.py
+++
@@ -1,9 +0,0 @@
-dictionary = open('dictionary.txt').read().splitlines()
-newdict = []
-#print dictionary
-for i in dictionary:
-	print i
-	if '-' not in i and "'" not in i:
-		newdict.append(i)
-print newdict
-open('test.txt', 'wb').write("\n".join(newdict))
