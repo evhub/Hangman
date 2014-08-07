0b2d19a64f5963b6af47f38f8dbb678e7d092113
evhub <evanjhub@gmail.com>
2014-08-07 12:30:56 -0700

    Uses my hangman AI

--- README.md
+++
@@ -1,9 +0,0 @@
-Hangman
-=======
-
-Hangman Game with API spec
-
-How to run:
-- Download hangman.py and dictionary.txt
-- Change the import "hangmanai" to your ai's filename
-- Run hangman.py
--- hangman.py
+++ hangman.py
@@ -1,144 +1,94 @@
-import random
-import string
-import operator
-from hangmanai import AI
-dictionary = open('dictionary.txt').read().splitlines()
-spaces=""
-wrongletters = 0
-HANGMANPICS = ['''
- 
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
- 
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-def handle_guess(guess): 
-	# -1 if not found
-	# -2 if won
-	# position if found
-	global spaces
-	global wrongletters
-	print spaces
-	rlist=[]
-	print HANGMANPICS[wrongletters]
-	nothing = raw_input("Press enter to guess")
-	print "-----------------------------"
-	pos = word.find(guess, 0)
-	if pos == -1:
-		wrongletters+=1
-		return -1
-	else:
-		spaces = list(spaces)
-		clist = []
-		for i in range(0,len(spaces)):
-			if i%2==0:
-				clist.append(spaces[i])
-		clist[pos] = guess
-		rlist.append(pos)
-		while(True):
-			if word.find(guess,pos+1)== -1:
-				break
-			pos = word.find(guess,pos+1)
-			rlist.append(pos)
-			clist[pos]=guess
-		won = "".join(clist)
-		if won==word:
-			print word
-			print "You win"
-			return -2
-		spaces = " ".join(clist)
-		if len(rlist)>1:
-			return rlist
-		else:
-			return pos
+#!/usr/bin/python
 
-while(True):
-	spaces = ""
-	wrongletters = 0
-	answer = raw_input('Want me to enter a word?')
-	if answer == 'n':
-		word = raw_input('Enter Word: ')
-	else:
-		word = random.choice(dictionary)
-	for i in range(0,len(word)):
-		spaces += "_ "
-	answer = raw_input("Do you want the computer to play?")
-	if answer == 'y':
-		ai = AI(word,dictionary)
-		while(True):
-			guess = ai.get_letter()
-			greturn = handle_guess(guess)
-			if type(greturn) is list:
-				for greturn in greturn:
-					status = ai.update(greturn,guess)
-					if status == -1:
-						print "I lose!"
-						break
-					elif status == 1:
-						print "I win!"
-						break
-			else:
-				status = ai.update(greturn,guess)
-				if status == -1:
-					print HANGMANPICS[wrongletters]
-					print "I lose!"
-					break
-				elif status == 1:
-					print "I win!"
-					break
+# Let's win at Hangman, shall we?
 
-	else:
-		while(True):
-			guess = raw_input('Guess a letter: ')
-			if guess(guess) == -2:
-				break
+# API Reference:
+# hangman(word_length[, dictionary_url]).guess()
+
+from __future__ import absolute_import, with_statement, print_function, unicode_literals
+import urllib
+
+def makestr(inputobj):
+    if isinstance(inputobj, unicode):
+        return inputobj
+    elif isinstance(inputobj, str):
+        return unicode(inputobj, "UTF")
+    elif isinstance(inputobj, list):
+        return "".join(map(makestr, inputobj))
+    else:
+        raise ValueError("Cannot convert to unicode "+repr(inputobj))
+
+def swaptuple(tuplelist):
+    out = []
+    for x,y in tuplelist:
+        out.append((y,x))
+    return out
+
+class hangman(object):
+    """A Hang Man AI."""
+    def guess(self):
+        """The Public API function. Returns a tuple: (guess_letter, answer_function) where
+    guess_letter = A single unicode char representing the AI's next guess.
+    answer_function = A function that should be called to respond to the AI's guess with arguments
+        answer_function(indexlist) where indexlist is a list of all the string indexes where that letter was found.
+        The answer_function will then return this function again for use in getting the next guess.
+    If the AI has found your word exactly, it will return that as the first argument and a function to print it as the second."""
+        self.clean()
+        if len(self.words) == 0:
+            raise ValueError("Your word doesn't exist in the dictionary!")
+        elif len(self.words) == 1:
+            return (self.words[0], lambda: print("Your word is "+self.words[0]+"."))
+        else:
+            letter = self.get_letter()
+            def _answer(indexes=[]):
+                if len(indexes) == 0:
+                    self.addno(letter)
+                else:
+                    for index in indexes:
+                        self.addyes(letter, index)
+                return self.guess
+            return (letter, _answer)
+    def __init__(self, word_length, dictionary_url="https://raw.githubusercontent.com/evhub/Hangman/master/dictionary.txt"):
+        """Call with the length of the word in question as the first argument, and the url where a dictionary can be accessed as the second."""
+        self.wordlen = int(word_length)
+        self.get_dictionary(dictionary_url)
+        self.yes = {}
+        self.no = ""
+    def get_dictionary(self, url=None):
+        url = makestr(url)
+        self.words = []
+        path = str(urllib.urlretrieve(url)[0])
+        with open(path, "rb") as f:
+            for line in f:
+                line = line.strip()
+                if self.wordlen == len(line):
+                    self.words.append(line)
+    def check(self, word):
+        for p,c in self.yes.iteritems():
+            if word[p] != c:
+                return False
+        for c in word:
+            if c in self.no:
+                return False
+        return True
+    def clean(self):
+        if len(self.yes) + len(self.no) > 0:
+            words, self.words = self.words, []
+            for word in words:
+                if self.check(word):
+                    self.words.append(word)
+    def get_letter(self):
+        yes = makestr(self.yes.values())
+        letters = {}
+        for word in self.words:
+            for c in word:
+                if c not in yes:
+                    if c in letters:
+                        letters[c] += 1
+                    else:
+                        letters[c] = 0
+        return max(swaptuple(letters.items()))[1]
+    def addyes(self, letter, pos):
+        self.yes[int(pos)] = makestr(letter)
+    def addno(self, letter):
+        self.no += makestr(letter)
